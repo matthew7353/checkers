9ed5ef49991abb495d286ef9572c37608cf30285
Mateusz Kunicki <matthew7353@gmail.com>
2023-05-24 14:57:07 +0200

    cleaning the code

--- game.rb
+++ game.rb
@@ -43,19 +43,7 @@ class Game
         pawns = @board.pawns(@turn)
         @can_beat = false
 
-        pawns.each{|x| @selected = x.square
-            case x.square % 8
-            when 0..1
-                @can_beat ||= (check_beat(@selected - 14) || check_beat(@selected + 18))
-            when 2..5
-                @can_beat ||= (check_beat(@selected - 14) || check_beat(@selected + 18))
-                @can_beat ||= (check_beat(@selected - 18) || check_beat(@selected + 14))
-            when 6..7
-                @can_beat ||= (check_beat(@selected - 18) || check_beat(@selected + 14))
-            end
-        }
-
-        @selected = false
+        pawns.each{ |x| @can_beat ||= check_possible_beatings(x.square) }
     end
 
     def check_move(square)
@@ -90,28 +78,23 @@ class Game
         if square >= 0 and square < 64
             case @selected % 8
             when 0..1
-                right.each{ |x| if square + x >= 0 && square + x < 64
-                    question ||= (@selected == square + x and @board.square_value(square) == '' and
-                    @board.square_value(square + x/2) != '' and @board.square_value(square + x/2) != @turn)
-                end}
+                right.each{ |x| question ||= beating_conditions(square, check_square, x)}
             when 2..5
-                right.each{ |x| if square + x >= 0 && square + x < 64
-                    question ||= (@selected == square + x and @board.square_value(square) == '' and
-                    @board.square_value(square + x/2) != '' and @board.square_value(square + x/2) != @turn)
-                end}
-                left.each{ |x| if square + x >= 0 && square + x < 64
-                    question ||= (@selected == square + x and @board.square_value(square) == '' and
-                    @board.square_value(square + x/2) != '' and @board.square_value(square + x/2) != @turn)
-                end}
+                right.each{ |x| question ||= beating_conditions(square, check_square, x)}
+                left.each{ |x| question ||= beating_conditions(square, check_square, x)}
             when 6..7
-                left.each{ |x| if square + x >= 0 && square + x < 64
-                    question ||= (@selected == square + x and @board.square_value(square) == '' and
-                    @board.square_value(square + x/2) != '' and @board.square_value(square + x/2) != @turn)
-                end}
+                left.each{ |x| question ||= beating_conditions(square, check_square, x)}
             end
         end
 
         question
+    end
+
+    def beating_conditions(square, check_square, x)
+        if check_square + x >= 0 && check_square + x < 64
+            (square == check_square + x and @board.square_value(check_square) == '' and
+            @board.square_value(check_square + x/2) != '' and @board.square_value(check_square + x/2) != @turn)
+        else false end
     end
 
     def beat(square)
